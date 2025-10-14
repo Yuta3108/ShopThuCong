9f965d9d266735e03605c7bb9a980c430f07470e
Yu3108 <toyotran0@gmail.com>
2025-10-14 22:11:39 +0700

    sua ty

--- backend/api/index.js
+++ backend/api/index.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api", userRoutes);
 
-app.get("/", (req, res) => {
+app.get("/check", (req, res) => {
   res.send("✅ Backend API đang chạy trên Vercel");
 });
 
