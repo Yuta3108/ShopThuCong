b82b2040ae01224253066053511fde768cf81338
Yu3108 <toyotran0@gmail.com>
2025-10-14 21:52:42 +0700

    test hosting lan 7

--- ShopThuCong/src/User/ChucNang/DangNhap.jsx
+++ ShopThuCong/src/User/ChucNang/DangNhap.jsx
@@ -15,7 +15,7 @@ function DangNhap() {
     e.preventDefault();
     setError("");
     try {
-      const response = await fetch(`/api/khachhang/dangnhap`, {
+      const response = await fetch(`http://localhost:5000/api/khachhang/dangnhap`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, matKhau: password }),
