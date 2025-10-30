65183b4295046570c23f911caf4b7703836f0a28
Yu3108 <toyotran0@gmail.com>
2025-10-30 15:17:25 +0700

    chỉnh đường dẫn

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,8 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes.js"; // nếu routes nằm ngoài thư mục api
+import userRoutes from "./routes/userRoutes.js"; 
 import userManageRoutes from "./routes/userManageRoutes.js";
+import categoryRoutes from "./routes/CategoryRoute.js";
+import productRoutes from "./routes/productsRoute.js";
+import attributesRoute from "./routes/attributesRoute.js";
+import attributeValuesRoute from "./routes/attributeValuesRoute.js";
+import variantAttributesRoute from "./routes/variantAttributesRoute.js";
 dotenv.config();
 
 const app = express();
@@ -11,6 +16,12 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.json());
 app.use("/api", userRoutes);
 app.use("/api/users", userManageRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/categories", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/attributes", attributesRoute);
+app.use("/api/attribute-values", attributeValuesRoute);
+app.use("/api/variant-attributes", variantAttributesRoute);
 app.get("/", (req, res) => {
   res.send("Backend API đang chạy trên Vercel");
 });
