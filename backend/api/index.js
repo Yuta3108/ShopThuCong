fb1523c61198edaf96ac05e866c5f762968b0f0b
Yu3108 <toyotran0@gmail.com>
2026-01-05 23:24:10 +0700

    sửa zalo

--- backend/api/index.js
+++ backend/api/index.js
@@ -15,12 +15,12 @@ import orderRoutes from "./routes/orderRoutes.js"
 import zaloPayRoutes from "./ZaloPay/zaloPayRoutes.js";
 import shippingRoutes from "./GHN/shippingRoutes.js";
 import { autoCancelPendingOrders } from "./config/AutoZalopay.js";
-autoCancelPendingOrders();
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
+autoCancelPendingOrders();
 
 app.use("/api", userRoutes);
 app.use("/api/users", userManageRoutes);
--- backend/server.js
+++ backend/server.js
@@ -15,13 +15,13 @@ import cartRoutes from "./api/routes/cartRoutes.js";
 import orderRoutes from "./api/routes/orderRoutes.js";
 import zaloPayRoutes from "./api/ZaloPay/zaloPayRoutes.js";
 import shippingRoutes from "./api/GHN/shippingRoutes.js";
-import { autoCancelPendingOrders } from ".api/config/AutoZalopay.js";
-autoCancelPendingOrders();
+import { autoCancelPendingOrders } from "./api/config/AutoZalopay.js";
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json({ limit: "20mb" }));
 app.use(express.urlencoded({ extended: true }));
+autoCancelPendingOrders();
 
 
 app.use("/api", userRoutes);
